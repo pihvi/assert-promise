fef2e643ccb0da9e8ff73fefd072f3cdd011e7a0
Antti Mattila <antti.mattila@reaktor.fi>
2013-07-22 22:15:06 +0300

    Refactor helper function for promise creation

--- test/example-failing-tests.js
+++ test/example-failing-tests.js
@@ -8,38 +8,42 @@ it('fail assert on non promise', function(done) {
 })
 
 it('fail basic promise', function(done) {
-  Q(2).delay(50).then(function(two) {
+  promise(2).then(function(two) {
     assert.equal(two, 3)
   }).done(done)
 })
 
 it('fail assert promise with equal', function(done) {
-  assertPromise.equal(Q(2).delay(50), 3).done(done)
+  assertPromise.equal(promise(2), 3).done(done)
 })
 
 it('fail assert promise with notEqual', function(done) {
-  assertPromise.notEqual(Q(3).delay(50), 3).done(done)
+  assertPromise.notEqual(promise(3), 3).done(done)
 })
 
 it('fail assert promise', function(done) {
-  assertPromise(Q(false).delay(50), "fail on false").done(done)
+  assertPromise(promise(false), "fail on false").done(done)
 })
 
 it('fail assert promise with message', function(done) {
-  assertPromise.equal(Q(2).delay(50), 3, 'this should fail').done(done)
+  assertPromise.equal(promise(2), 3, 'this should fail').done(done)
 })
 
 it('fail assert promise with own assert function', function(done) {
   var alwaysFail = function(actual, expected, message) {
     assert(false, 'Failing anyway. Got ' + actual + ' and ' + expected + ' with message: ' + message)
   }
-  assertPromise.withFn(Q(1).delay(50), true, 'this should fail', alwaysFail).done(done)
+  assertPromise.withFn(promise(1), true, 'this should fail', alwaysFail).done(done)
 })
 
 it('fail assert promise with other assert function', function(done) {
-  assertPromise.withFn(Q(1).delay(50), true, 'using strict, should fail', assert.strictEqual).done(done)
+  assertPromise.withFn(promise(1), true, 'using strict, should fail', assert.strictEqual).done(done)
 })
 
 it('fail assert mocha-as-promised', function() {
-  return assertPromise.equal(Q(2).delay(50), 3)
+  return assertPromise.equal(promise(2), 3)
 })
+
+function promise(val) {
+  return Q(val).delay(50)
+}
--- test/example-passing-tests.js
+++ test/example-passing-tests.js
@@ -4,7 +4,7 @@ var assert = require('assert')
 var Q = require('q')
 
 it('basic passing promise', function(done) {
-  Q(2).delay(50).then(function(two) {
+  promise(2).then(function(two) {
     assert.equal(two, 2)
   }).done(done)
 })
@@ -13,38 +13,42 @@ it('assert passing promise with own assert function', function(done) {
   var assertOneLess = function(actual, expected, message) {
     assert.equal(actual + 1, expected, message)
   }
-  assertPromise.withFn(Q(2).delay(50), 3, 'this should pass', assertOneLess).done(done)
+  assertPromise.withFn(promise(2), 3, 'this should pass', assertOneLess).done(done)
 })
 
 it('assert.equal passing promise', function(done) {
-  assertPromise.equal(Q(1).delay(50), 1).done(done)
+  assertPromise.equal(promise(1), 1).done(done)
 })
 
 it('assert.notEqual passing promise', function(done) {
-  assertPromise.notEqual(Q(1).delay(50), 2).done(done)
+  assertPromise.notEqual(promise(1), 2).done(done)
 })
 
 it('assert.ok passing promise', function(done) {
-  assertPromise.ok(Q(1)).done(done)
+  assertPromise.ok(promise(1)).done(done)
 })
 
 it('assert() passing promise', function(done) {
-  assertPromise(Q(1)).done(done)
+  assertPromise(promise(1)).done(done)
 })
 
 it('assert passing mocha-as-promised', function() {
-  return assertPromise.equal(Q(2).delay(50), 2)
+  return assertPromise.equal(promise(2), 2)
     .then(function() {
-      return assertPromise.notEqual(Q(3).delay(50), 2)
+      return assertPromise.notEqual(promise(3), 2)
     }).then(function() {
-      return assertPromise(Q(true).delay(50))
+      return assertPromise(promise(true))
     }).then(function() {
-      return assertPromise.deepEqual(Q({a: 1}).delay(50), {a: true})
+      return assertPromise.deepEqual(promise({a: 1}), {a: true})
     }).then(function() {
-      return assertPromise.notDeepEqual(Q({a: 2}).delay(50), {a: true})
+      return assertPromise.notDeepEqual(promise({a: 2}), {a: true})
     }).then(function() {
-      return assertPromise.strictEqual(Q(1).delay(50), 1)
+      return assertPromise.strictEqual(promise(1), 1)
     }).then(function() {
-      return assertPromise.notStrictEqual(Q(true).delay(50), 1)
+      return assertPromise.notStrictEqual(promise(true), 1)
     })
 })
+
+function promise(val) {
+  return Q(val).delay(50)
+}
